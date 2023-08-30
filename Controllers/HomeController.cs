910588f4ecc5903ea6b9eb03e517773cd2c74f6e
Jigar1992 <jigar.thakkar@tatvasoft.com>
2023-08-30 20:09:05 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TestAvinashContext _testAvinashContext;
+
+        public HomeController(ILogger<HomeController> logger, TestAvinashContext testAvinashContext)
+        {
+            _logger = logger;
+            _testAvinashContext = testAvinashContext;
+            //_testAvinashContext = testAvinashContext;
+        }
+
+        public IActionResult Index()
+        {
+            //TestAvinashContext _testAvinashContext = new TestAvinashContext();
+            var a = _testAvinashContext.UserAddresses.ToList();
+
+            var abc = _testAvinashContext.Users.ToList();
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public virtual ICollection<UserAddress> UserAddresses { get; set; } = new List<UserAddress>();
+}
